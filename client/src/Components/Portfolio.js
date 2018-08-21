f8f0f4fc42c6bbe422a160e876fcceb80b61631e
Chooster <jcooleycoder@gmail.com>
2018-08-21 15:42:31 -0700

    Initial Website Commit

---
+++ client/src/Components/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="page-wrapper">
+      <div className="Portfolio">
+        <h1 id="page-title">Portfolio</h1>
+        <a
+          href="https://drive.google.com/file/d/1ov4HQIvLgXHu3v_PSofU6uu1iLBPBoKA/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          (Resume)
+        </a>
+      </div>
+    </div>
+  );
+};
